3ba1319947c7e11ff4c071f6df8eaaf3fa1509e4
paulna45 <57304712+paulna45@users.noreply.github.com>
2019-12-03 10:18:50 -0500

    functional game i hope

--- AgarObjects.py
+++ AgarObjects.py
@@ -1,6 +1,13 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import random
+import random, math, decimal
+
+
+# from https://www.cs.cmu.edu/~112/notes/notes-variables-and-functions.html#HelperFunctions
+def roundHalfUp(d):
+    # Round to nearest with ties going away from zero.
+    rounding = decimal.ROUND_HALF_UP
+    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
 
 # from https://www.cs.cmu.edu/~112/notes/notes-graphics-part2.html#customColors
 def rgbString(red, green, blue):
@@ -17,31 +24,67 @@ class Agar(object):
         self.g = random.randint(0,255)
         self.b = random.randint(0,255)
         self.color = rgbString(self.r, self.g, self.b)
-        self.isEaten = False
     
     def __hash__(self):
         return hash(self.getHashables())
+    def __repr__(self):
+        return f'({self.r},{self.g},{self.b}) at ({self.cx}, {self.cy})'
+    def __eq__(self, other):
+        return isinstance(other, Agar)
 
     def move(self):
-        self.cy += self.dy
         self.cx += self.dx
+        if self.cx - self.radius < self.mode.leftBound:
+            self.cx = self.mode.leftBound + self.radius
+            self.dx = 0
+            print('boop')
+        elif self.cx + self.radius > self.mode.rightBound:
+            self.cx = self.mode.rightBound - self.radius
+            self.dx = 0
+            print('bop')
+        
+        self.cy += self.dy
+        if self.cy - self.radius < self.mode.upperBound:
+            self.cy = self.mode.upperBound + self.radius
+            self.dy = 0
+            print('blip')
+        elif self.cy + self.radius > self.mode.lowerBound:
+            self.cy = self.mode.lowerBound - self.radius
+            self.dy = 0
+            print('beep')
     
     def checkIfCanEat(self, other):
-        return (other.radius < self.radius * 1.2) and \
+        if (other.radius < self.radius) and \
                (other.cx - other.radius) >= (self.cx - self.radius) and \
                (other.cx + other.radius) <= (self.cx + self.radius) and \
                (other.cy - other.radius) >= (self.cy - self.radius) and \
-               (other.cy + other.radius) <= (self.cy + self.radius)
+               (other.cy + other.radius) <= (self.cy + self.radius):
+            return True
+        else: return False
 
     def eatAgar(self, other):
-        self.radius += other.radius
-        other.isEaten = False
+        self.radius += roundHalfUp(math.sqrt(other.radius))
+
+class Blob(Agar):
+    def __init__(self, mode, cx, cy):
+        super().__init__(mode, cx, cy)
+        self.radius = random.randint(2, 5)
+        self.color = rgbString(self.r, self.g, self.b)
+
+    def getHashables(self):
+        return (self.cx, self.cy)
+
+    def draw(self, canvas):
+        canvas.create_oval(self.cx - self.radius, self.cy - self.radius, 
+                           self.cx + self.radius, self.cy + self.radius, 
+                           fill=self.color, outline='Yellow')
 
 class Player(Agar):
     def __init__(self, mode, cx, cy):
         super().__init__(mode, cx, cy)
         self.radius = 10
         self.color = rgbString(255,0,0)
+        self.score = self.mode.app.score
     
     def getHashables(self):
         return (self.name,)
@@ -49,6 +92,11 @@ class Player(Agar):
     def draw(self, canvas):
         canvas.create_oval(self.cx - self.radius, self.cy - self.radius, 
                            self.cx + self.radius, self.cy + self.radius, fill=self.color)
+    
+    def eatAgar(self, other):
+        self.radius += roundHalfUp(math.sqrt(other.radius))
+        self.mode.app.score += other.radius
+        self.score = self.mode.app.score
 
 class Enemy(Agar):
     def __init__(self, mode, cx, cy, radius):
@@ -61,3 +109,103 @@ class Enemy(Agar):
     def draw(self, canvas):
         canvas.create_oval(self.cx - self.radius, self.cy - self.radius, 
                            self.cx + self.radius, self.cy + self.radius, fill=self.color)
+
+    def changeDir(self):
+        self.dx = random.randrange(-1,1)
+        self.dy = random.randrange(-1,1)
+
+    #def move(self):
+        
+
+class AggressiveEnemy(Enemy):
+    def __init__(self, mode, cx, cy, radius):
+        super().__init__(mode, cx, cy, radius)
+        self.color = rgbString(self.r, 0, 0)
+
+    def changeDir(self):
+        if self.radius > self.mode.player.radius:
+            self.dx = (self.mode.player.cx - self.cx) / (self.mode.width / 4)
+            self.dy = (self.mode.player.cy - self.cy) / (self.mode.height / 4)
+        else:
+            closestSmallerEnemy = None
+            closestSmallerEnemyDistance = ''
+            for enemy in self.mode.enemies:
+                if self.radius <= enemy.radius:
+                    continue #checks for larger enemies and if self == enemy
+                distance = math.sqrt((self.cx + enemy.cx)**2 + (self.cy + enemy.cy)**2)
+                #print(distance)
+                if closestSmallerEnemyDistance == '':
+                    closestSmallerEnemyDistance = distance
+                    closestSmallerEnemy = enemy
+                elif distance < closestSmallerEnemyDistance:
+                    closestSmallerEnemyDistance = distance
+                    closestSmallerEnemy = enemy
+            #print(closestSmallerEnemy, closestSmallerEnemyDistance)
+            if closestSmallerEnemy == None:
+                self.dx = 0
+                self.dy = 0
+            else:
+                self.dx = (closestSmallerEnemy.cx - self.cx) / (self.mode.width / 4)
+                self.dy = (closestSmallerEnemy.cy - self.cy) / (self.mode.height / 4)
+
+class TimidEnemy(Enemy):
+    def __init__(self, mode, cx, cy, radius):
+        super().__init__(mode, cx, cy, radius)
+        self.color = rgbString(0, 0, self.b)
+
+    def changeDir(self):
+        if self.radius < self.mode.player.radius:
+            self.dx = (self.cx - self.mode.player.cx) / (self.mode.width / 4)
+            self.dy = (self.cy - self.mode.player.cy) / (self.mode.height / 4)
+        else:
+            closestLargerEnemy = None
+            closestLargerEnemyDistance = ''
+            for enemy in self.mode.enemies:
+                if self.radius >= enemy.radius:
+                    continue # skips if other enemy is smaller or same size (includes self)
+                distance = math.sqrt((self.cx + enemy.cx)**2 + (self.cy + enemy.cy)**2)
+                #print(distance)
+                if closestLargerEnemyDistance == '':
+                    closestLargerEnemyDistance = distance
+                    closestLargerEnemy = enemy
+                elif distance < closestLargerEnemyDistance:
+                    closestLargerEnemyDistance = distance
+                    closestLargerEnemy = enemy
+            #print(closestSmallerEnemy, closestSmallerEnemyDistance)
+            if closestLargerEnemy == None:
+                self.dx = 0
+                self.dy = 0
+            else:
+                self.dx = (self.cx - closestLargerEnemy.cx) / (self.mode.width / 4)
+                self.dy = (self.cy - closestLargerEnemy.cy) / (self.mode.height / 4)
+
+class PassiveEnemy(Enemy):
+    def __init__(self, mode, cx, cy, radius):
+        super().__init__(mode, cx, cy, radius)
+        self.color = rgbString(0, self.g, 0)
+
+    def changeDir(self):
+        if self.radius < self.mode.player.radius:
+            self.dx = (self.cx - self.mode.player.cx) / (self.mode.width / 4)
+            self.dy = (self.cy - self.mode.player.cy) / (self.mode.height / 4)
+        else:
+            closestLargerEnemy = None
+            closestLargerEnemyDistance = ''
+            for enemy in self.mode.enemies:
+                if self.radius >= enemy.radius:
+                    continue #checks for smaller enemies and if self == enemy
+                distance = math.sqrt((self.cx + enemy.cx)**2 + (self.cy + enemy.cy)**2)
+                #print(distance)
+                if closestLargerEnemyDistance == '':
+                    closestLargerEnemyDistance = distance
+                    closestLargerEnemy = enemy
+                elif distance < closestLargerEnemyDistance:
+                    closestLargerEnemyDistance = distance
+                    closestLargerEnemy = enemy
+            #print(closestSmallerEnemy, closestSmallerEnemyDistance)
+            if closestLargerEnemy == None:
+                self.dx = 0
+                self.dy = 0
+            else:
+                self.dx = (self.cx - closestLargerEnemy.cx) / (self.mode.width / 4)
+                self.dy = (self.cy - closestLargerEnemy.cy) / (self.mode.height / 4)
